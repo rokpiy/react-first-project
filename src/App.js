d57f63de765f05cdf0c157f73a253e8b0421efab
rokpiy <inyoung@aristo.so>
2025-10-19 14:38:58 +0900

    Todos

--- src/App.js
+++ src/App.js
@@ -2,23 +2,33 @@ import Button from './Button';
 import styles from './App.module.css';
 import { useState, useEffect } from 'react';
 
-function Hello() {
-  useEffect(() => {
-    console.log("hi:)");
-    return () => console.log("bye:)");
-  }, []);
-  return <h1>Hello</h1>;
-}
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-  
+ const [value, setValue] = useState("");
+ const [todos, setTodos] = useState([]);
+ const onChange = (event) => setValue(event.target.value);
+ const onSubmit = (event) => {
+  event.preventDefault();
+  if (value === "") {
+    return;
+  }
+  setTodos((currentArray) => [value, ...currentArray]);
+  setValue("");
+  console.log(todos);
+ }
   return (
     <div>
-      {showing ? <Hello /> : null}
-    <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({todos.length})</h1>
+      <form onSubmit={onSubmit}>
+      <input value={value} onChange={onChange} type="text" placeholder="Write your to do.." />
+      <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {todos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
-
 export default App;
